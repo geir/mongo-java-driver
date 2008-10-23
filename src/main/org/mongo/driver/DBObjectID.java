ba05e05cbace1e9529b17a1bf769333f4dd40b0d
Geir Magnusson Jr <geir@pobox.com>
2008-10-23 00:16:33 -0400

    Add Apache License to all files

--- src/main/org/mongo/driver/DBObjectID.java
+++ src/main/org/mongo/driver/DBObjectID.java
@@ -1,3 +1,19 @@
+/**
+*      Copyright (C) 2008 Geir Magnusson Jr
+*  
+*    Licensed under the Apache License, Version 2.0 (the "License");
+*    you may not use this file except in compliance with the License.
+*    You may obtain a copy of the License at
+*  
+*       http://www.apache.org/licenses/LICENSE-2.0
+*  
+*    Unless required by applicable law or agreed to in writing, software
+*    distributed under the License is distributed on an "AS IS" BASIS,
+*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*    See the License for the specific language governing permissions and
+*    limitations under the License.
+*/
+
 package org.mongo.driver;
 
 import java.util.UUID;
@@ -6,6 +22,10 @@ import java.nio.ByteOrder;
 
 /**
  *   UUID that's quasi-compatible with the 10gen JS appserver ObjectID
+ *
+ *   TODO - this needs to be fixed.  It would be nice to scrunch it down
+ *   to 12 bytes so we don't have to re-wtite it, but it needs to be fixed
+ *   either way
  */
 public class DBObjectID {
 
