301b028b0dbcf5a8ed4183296dcd12b2de47d8ab
Geir Magnusson Jr <geir@pobox.com>
2008-12-07 22:32:17 -0500

    mongo.groovy : update to use dyn driver

--- src/examples/groovy/mongo.groovy
+++ src/examples/groovy/mongo.groovy
@@ -14,33 +14,20 @@
 *    limitations under the License.
 */
 
-import org.mongo.driver.impl.DBImpl
-import  org.mongo.driver.MongoDoc
+import org.mongodb.driver.dyn.Mongo
 
-def db = new DBImpl("groovy")
+def mongo = new Mongo();
 
-def coll = db.getCollection("test", true)
+def db = mongo.getDB("groovy")
 
+def coll = db.getCollection("test")
 coll.clear()
 
-def doc = new MongoDoc()
-
 for (i in 1..5) {
-  doc["a"] = i + 1
-  coll.insert doc
+  coll.insert(["a" : i+1])
 }
 
 coll.find().each { i -> println i }
 
-// now use native map for objects and selectors
 
-coll = db.getCollection("test2", true)
-
-coll.clear()
-
-coll.insert( ["b" : 1 ])
-coll.insert( ["b" : 2 ])
-
-coll.find().each { i -> println i }
-
-coll.find(["b" : 2]).each { i -> println i }
+coll.find(["a" : 2]).each { i -> println i }
