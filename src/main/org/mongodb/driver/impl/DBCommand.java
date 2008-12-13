3132a668246822ffeead9c381a29600f119f4b4c
Geir Magnusson Jr <geir@pobox.com>
2008-12-13 11:50:00 -0500

    query vs commands : de-hork the API regarding queries and commands. They are the same message to the database, but for a command, mongo expects the 'verb' to be the first key in the BSON doc, so you can't have a general query structure.  Will try to get this fixed in Mongo.

---
+++ src/main/org/mongodb/driver/impl/DBCommand.java
@@ -0,0 +1,37 @@
+/**
+*      Copyright (C) 2008 Geir Magnusson Jr
+*
+*    Licensed under the Apache License, Version 2.0 (the "License");
+*    you may not use this file except in compliance with the License.
+*    You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*    Unless required by applicable law or agreed to in writing, software
+*    distributed under the License is distributed on an "AS IS" BASIS,
+*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*    See the License for the specific language governing permissions and
+*    limitations under the License.
+*/
+
+package org.mongodb.driver.impl;
+
+import org.mongodb.driver.MongoDBException;
+import org.mongodb.driver.ts.MongoDoc;
+import org.mongodb.driver.ts.MongoSelector;
+import org.mongodb.driver.ts.DBQuery;
+
+/**
+ *  A query for Mongo
+ */
+public class DBCommand extends DBQuery {
+
+    public DBCommand(MongoSelector sel) {
+        _querySelector = sel;
+        setNumberToReturn(1); // tis required to be only 1 return, technally -1?
+    }
+
+    public MongoDoc getCompleteQuery() throws MongoDBException {
+        return _querySelector;
+    }
+}
